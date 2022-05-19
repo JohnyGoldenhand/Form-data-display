3acd07e740fc1d01103b3a2180fa4b1b4640fab9
Remigiusz Mikolajewicz <r.mikolajewicz@interia.pl>
2022-05-19 12:27:45 +0200

    fixed delayed data display

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [name, setName] = useState<string>('')
+  const [description, setDescription] = useState<string>('')
+  const [category, setCategory] = useState<string>('')
+  const [watering, setWatering] = useState<string>('')
+  const [prefferences, setPreferences] = useState<string>('')
+
+  interface PlantProps {
+    name: string;
+    description: string;
+    category: string;
+    watering: string;
+    prefferences: string;
+  }
+
+
+
+  const [plantList, setPlantList] = useState <Array<PlantProps>>([])
+
+  const handleChangeName = (e: React.ChangeEvent<any>) => {
+    e.preventDefault();
+    const value = (e.target as HTMLInputElement).value;
+    setName(value)
+  }
+  const handleChangeDescription = (e: React.ChangeEvent<any>) => {
+    e.preventDefault();
+    const value = (e.target as HTMLInputElement).value;
+    setDescription(value)
+  }
+  const handleChangeCategory = (e: React.ChangeEvent<any>) => {
+    e.preventDefault();
+    const value = (e.target as HTMLInputElement).value;
+    setCategory(value)
+  }
+  const handleChangeWatering = (e: React.ChangeEvent<any>) => {
+    e.preventDefault();
+    const value = (e.target as HTMLInputElement).value;
+    setWatering(value)
+  }
+
+  const handleChangePrefferences = (e: React.ChangeEvent<any>) => {
+    e.preventDefault();
+    const value = (e.target as HTMLInputElement).value;
+    setPreferences(value)
+  }
+
+
+  const handleSubmit = (e: React.FormEvent) => {
+    setPlantList([...plantList, {name:name, description:description, category:category, watering:watering, prefferences:prefferences}]);
+    e.preventDefault();
+    console.log(plantList);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <form onSubmit={handleSubmit}>
+        <div>
+          <input type="text" onChange={handleChangeName} placeholder='name' value={name}/>
+          <input type="text" onChange={handleChangeDescription} placeholder='description' value={description}/>
+          <input type="text" onChange={handleChangeCategory} placeholder='category' value={category}/>
+          <input type="text" onChange={handleChangeWatering} placeholder='watering'value={watering}/>
+          <input type="text" onChange={handleChangePrefferences} placeholder='prefferences' value={prefferences}/>
+          <button  type="submit">Click</button>
+        </div>
+      </form>
+      <div>
+            {
+              plantList.map((plant, index) =>
+                <table key={index}>
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Category</th>
+                        <th>Watering</th>
+                        <th>Prefferences</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>{index}</td>
+                        <td>{plant.name}</td>
+                        <td>{plant.description}</td>
+                        <td>{plant.category}</td>
+                        <td>{plant.watering}</td>
+                        <td>{plant.prefferences}</td>
+                      </tr> 
+                    </tbody>
+
+                </table>
+)
+            }
+      </div>
     </div>
+
   );
 }
 
